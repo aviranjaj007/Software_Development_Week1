25dc735e18704e3047beea049d9abe381ffd20ca
Aviranjan <aviranjand@gmail.com>
2026-03-09 23:36:39 +0530

    Print Hello World

--- day1.java
+++ day1.java
@@ -1,5 +1,13 @@
-public class Main {
+/* public class Day1 {
     public static void main(String[] args) {
         System.out.println("Week 1 - Software Development Journey");
     }
 }
+ */
+
+    // Print Hello World
+/* public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+} */
